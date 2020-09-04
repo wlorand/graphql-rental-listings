fccad332b9c339f3f338944874835774534e8b05
Will Lorand <bill@milemarker3.org>
2020-09-03 20:51:34 -0700

    added some REST routes

--- v1/server/src/index.ts
+++ v1/server/src/index.ts
@@ -6,12 +6,38 @@
 import express from 'express';
 const PORT = process.env.port || 9000;
 
+// body-parser
+import bodyParser from 'body-parser';
+
+// misc vars
 const one = 1;
+
+// import mock data
+import { listings } from './listings';
 
 const app = express();
 
+// middleware - recall order here is imp!
+// and should be before any routes which are immed. followed
+app.use(bodyParser.json());
+
 app.get('/', (_req, res) => {
   res.send(`Hello ${one} Express World!`);
+});
+
+// GET listings
+app.get('/listings', (_req, res) => {
+  res.send(listings);
+});
+
+// Delete Listing via POST
+app.get('/delete-listing/:id', (req, res) => {
+  const id: string = req.params.id;
+  // remove listing
+  const newListings = listings.filter((listing) => listing.id !== id);
+  if (listings.length === newListings.length)
+    res.send('no such id found - failed to delete listing');
+  res.send(newListings);
 });
 
 app.listen(PORT, () => {
